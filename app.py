689b2146577b030e097f58e3310ff996adc98015
Johan Thelin <e8johan@gmail.com>
2021-03-22 17:38:03 +0100

    The little engineer got tired, so let's skip the camera for now

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from queue import Queue
 from adafruit_servokit import ServoKit
 
 import picamera
-import cv2
 import socket
 
 import io
@@ -121,21 +120,6 @@ def info_stream():
         while True:
             yield "data: {}\n\n".format(msg_queue.get())
     return Response(the_stream(), mimetype="text/event-stream")
-
-def gen():
-    """Video streaming generator function."""
-    while True:
-        rval, frame = vc.read()
-        cv2.imwrite('t.jpg', frame)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
-
-
-@app.route('/camera')
-def camera():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
